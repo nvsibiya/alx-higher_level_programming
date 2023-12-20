e3dfe5559dea8d75039804d5d1ae8ab2457a0d23
nvsibiya <nvsibiya0@gmail.com>
2023-12-20 17:46:37 +0000

    Write a function that raises a name exception with a message.

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 
 def raise_exception_msg(message=""):
-    """Raises a name exception with a message."""
-    try:
-        # Raise a NameError with the specified message
-        raise NameError(message)
-    except NameError as e:
-        # If a NameError occurs, print the error message
-        print(f"NameError: {e}")
-
-# Call the function with a custom message
-raise_exception_msg("This is a custom error message.")
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
